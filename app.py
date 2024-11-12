ae0ba3abbacf8f9ea0db0b891bcd9b3999659281
MeredithWiniarski <88870057+MeredithWiniarski@users.noreply.github.com>
2024-11-12 15:07:27 -0800

    test

---
+++ app.py
@@ -0,0 +1,23 @@
+
+import flask
+import json
+import urllib
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+
+
+
+def is_it_raining_in_seattle():
+    with urllib.request.urlopen('https://depts.washington.edu/ledlab/teaching/is-it-raining-in-seattle/') as response:
+        is_it_raining_in_seattle = response.read().decode()
+
+    if is_it_raining_in_seattle == "true":
+        return "<h1>Yes</h1>"
+    else:
+        return "<h1>No</h1>"
+
+
